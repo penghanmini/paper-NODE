954b3fd8357eae9dfbee772b20b08adbb0451516
Panda <scdyph1120@163.com>
2019-01-18 18:10:48 +0800

    20190118

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,16 @@ var mysql = require('./sqlUtil');
 
 /* GET home page. */
 
+router.get('/category', function(req, res) {
+    const sql = 'SELECT * FROM category';
+    mysql.dbConnection(sql,[],function (err,data) {
+        res.send({
+            data: data,
+            message: 'success',
+            status: 0,
+        });
+    })
+});
 
 router.get('/index/carousel', function(req, res) {
   const sql = 'SELECT * FROM carousellist WHERE belong = 1';
@@ -15,6 +25,16 @@ router.get('/index/carousel', function(req, res) {
       });
   })
 });
+router.get('/index/market',function (req, res) {
+    const sql = 'SELECT * FROM smallimg WHERE category = "category_0" and categorydetail = 1';
+    mysql.dbConnection(sql,[],function (err,data) {
+        res.send({
+            data: data,
+            message: 'success',
+            status: 0,
+        });
+    })
+})
 router.get('/livingHome/carousel', function(req, res) {
     const sql = 'SELECT * FROM carousellist WHERE belong = 2';
     mysql.dbConnection(sql,[],function (err,data) {
